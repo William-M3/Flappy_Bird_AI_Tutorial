813948fdc03bcd6076e3487ae3048a47c9aa9ea8
Will-23453 <williamtmurphyiii@gmail.com>
2023-10-26 16:45:04 -0400

    Screen correct along with physics engine

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 import pygame
+from flappy_bird_game_files.Bird_Class import Bird
 import neat
+
 import time
 import os
 import random
 
-WIN_WIDTH = 600
+# Screen Size
+WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 # import assets set up
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bird1.png"))),
-             pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bird2.png"))),
-             pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bird3.png")))]
-
 PIPE_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "pipe.png")))]
 
 BASE_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "base.png")))]
-BG_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bg.png")))]
+BG_IMGS = pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bg.png")))
 
-class Bird:
-    IMGS = BIRD_IMGS
-    MAX_Rotation = 25
-    ROT_VEL = 20
-    ANIMATION_TIME = 5
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.tilt = 0
-        self.tick_count = 0
-        self.vel = 0
-        self.height = self.y
-        self.img_count = 0
-        self.img = self.IMGS[self.img_count]
+def draw_window(win, bird):
+    win.blit(BG_IMGS, (0, 0))
+    bird.draw(win)
+    pygame.display.update()
 
-    def jump(self):
-        self.vel = 10.5
+def main():
+
+    new_bird = Bird(200, 200)
+    new_win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        new_bird.move()
+        draw_window(win=new_win, bird=new_bird)
+
+    pygame.quit()
+    quit()
+
+# Main function call
+main()
